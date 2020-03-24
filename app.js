9799c572d92f89e254e70b2d717bbe9e9162e26b
mmezgin <49004855+mmezgin@users.noreply.github.com>
2020-03-24 16:26:52 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,14 @@ const io = socketio.listen(server);
 io.sockets.on('connection', (socket) => {
     console.log('User Connected !');
 
+    socket.on('sendHello', () => {
+        console.log('Hi bro');
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected !');
     });
+    setTimeout(() => {
+        socket.emit('say hi');
+    }, 2000);
 });
